d1b4715b899eb43ecf3f9660944b8694dd74cfc2
s950ms20 <przemyslaw.plucinski950@gmail.com>
2019-06-30 23:44:21 +0200

    Some changes after integration with eslint

--- src/App.js
+++ src/App.js
@@ -1,13 +1,6 @@
 import React from 'react';
 import './App.scss';
-import { Playground } from './components/Playground'
+import Playground from './components/Playground';
 
-function App() {
-	return (
-	<div className = "App" >
-		<Playground />
-		</div >
-	);
-}
-
+function App() { return (<div className="App"><Playground /></div>); }
 export default App;
--- src/components/Playground.js
+++ src/components/Playground.js
@@ -1,115 +1,159 @@
 import * as React from 'react';
-import Select, {
+import Select from '@atlaskit/select';
 // components,
 // createFilter,
 // mergeStyles,
 // AsyncSelect,
 // CheckboxSelect,
 // CountrySelect,
-	// RadioSelect
-	// CreatableSelect,
-	// AsyncCreatableSelect,
-	// PopupSelect,
-} from '@atlaskit/select';
+// RadioSelect
+// CreatableSelect,
+// AsyncCreatableSelect,
+// PopupSelect,
 
 import Button, { ButtonGroup } from '@atlaskit/button';
 
-export const Playground = (props) => {
-    const [ selectedAttachmentsLeft, setSelectedAttachmentsLeft ] = React.useState([]);
-    const [selectedAttachmentsRight, setSelectedAttachmentsRight] = React.useState([]);
+const Playground = () => {
+const [selectedAttachmentsLeft, setSelectedAttachmentsLeft] = React.useState([]);
+const [selectedAttachmentsRight, setSelectedAttachmentsRight] = React.useState([]);
 
-	const options = [
-		{
-			value: 'chocolate',
-			label: 'Chocolate',
-			attachments: [ 'chocolate 1', 'chocolate 2', 'chocolate 3' ]
-		},
-		{
-			value: 'strawberry',
-			label: 'Strawberry',
-			attachments: [ 'strawberry 1', 'strawberry 2', 'strawberry 3' ]
-		},
-		{
-			value: 'vanilla',
-			label: 'Vanilla',
-			attachments: [ 'vanilla 1', 'vanilla 2', 'vanilla 3' ]
-		}
-    ];
-    
-    const copyAttachments = () => {
-        const leftBox = selectedAttachmentsLeft;
-        const rightBox = selectedAttachmentsRight;
-        const updatedBox = [...rightBox, ...leftBox];
-        setSelectedAttachmentsRight(updatedBox);
-    }
+const uid = () => Number(Math.floor(Math.random() * 142626600));
 
-    const deleteSelectedAttachment = (selectedBox,idx) => {
-        const box = selectedBox;
-       const updatedBox = box.filter(
-           att => att !== box[idx]
-       )
-       selectedBox === selectedAttachmentsLeft
-       ? setSelectedAttachmentsLeft(updatedBox)
-       : setSelectedAttachmentsRight(updatedBox)
-    }
+const options = [
+{
+value: 'chocolate',
+label: 'Chocolate',
+id: uid(),
+attachments: [
+{
+value: 'chocolate 1',
+id: uid()
+},
+{
+value: 'chocolate 2',
+id: uid()
+},
+{
+value: 'chocolate 3',
+id: uid()
+}
+]
+},
 
-	return (
-		<React.Fragment>
-			<h1> Playground </h1>
-			<div className="boxes-group">
-				<div className="box">
-					<Select
-						options={options}
-						onChange={(event) => {
-							setSelectedAttachmentsLeft(event.attachments);
-						}}
-					/>
-				</div>
-				<div className="box arrow-box">=></div>
-				<div className="box">
-                    <Select
-                        options={options} 
-                        onChange = {
-                            (event) => {
-                                setSelectedAttachmentsRight(event.attachments);
-                    						    }
-                    						}
-                    />
-				</div>
-			</div>
-			<div className="boxes-group">
-				<div className="fake-attachment-box">
-					<ul>
-                        {selectedAttachmentsLeft
-                        .map((attachment, idx) => {
-                            return <li key={idx}>{attachment}
-                            <Button 
-                            onClick={()=>deleteSelectedAttachment(selectedAttachmentsLeft, idx)}
-                            >Delete</Button>
-                            </li>;
-						})}
-					</ul>
-				</div>
-				<div className="fake-attachment-box">
-					<ul>
-                        {selectedAttachmentsRight
-                        .map((attachment, idx) => {
-							return <li key={idx}>{attachment}
-                            <Button 
-                            onClick={()=>deleteSelectedAttachment(selectedAttachmentsRight, idx)}
-                            >Delete</Button>
-                            </li>;
-						})}
-					</ul>
-				</div>
-			</div>
-            <div className="buttons-group">
-                <ButtonGroup>
-                    <Button onClick={()=>{copyAttachments()}}>Copy</Button>
-                    <Button>Cancel</Button>
-                    <Button>Acept</Button>
-            </ButtonGroup>
-            </div>
-		</React.Fragment>
-	);
+{
+value: 'strawberry',
+label: 'Strawberry',
+id: uid(),
+attachments: [
+  {
+  value: 'strawberry 1',
+  id: uid()
+  },
+  {
+  value: 'strawberry 2',
+  id: uid()
+  },
+  {
+  value: 'strawberry 3',
+  id: uid()
+  }
+  ]
+},
+{
+value: 'vanilla',
+label: 'Vanilla',
+id: uid(),
+attachments: [
+  {
+  value: 'vanilla 1',
+  id: uid()
+  },
+  {
+  value: 'vanilla 2',
+  id: uid()
+  },
+  {
+  value: 'vanilla 3',
+  id: uid()
+  }
+  ]
+}
+];
+
+const copyAttachments = () => {
+const leftBox = selectedAttachmentsLeft;
+const rightBox = selectedAttachmentsRight;
+const updatedBox = [...rightBox, ...leftBox];
+setSelectedAttachmentsRight(updatedBox);
 };
+
+const deleteSelectedAttachment = (selectedBox, idx) => {
+const box = selectedBox;
+const updatedBox = box.filter(att => att !== box[idx]);
+if (selectedBox === selectedAttachmentsLeft) {
+ setSelectedAttachmentsLeft(updatedBox);
+} else { setSelectedAttachmentsRight(updatedBox); }
+};
+
+return (
+  <React.Fragment>
+    <h1> Playground </h1>
+    <div className="boxes-group">
+      <div className="box">
+        <Select
+          options={options}
+          onChange={(event) => {
+setSelectedAttachmentsLeft(event.attachments);
+}}
+        />
+      </div>
+      <div className="box arrow-box"> to </div>
+      <div className="box">
+        <Select
+          options={options}
+          onChange={(event) => {
+setSelectedAttachmentsRight(event.attachments);
+}}
+        />
+      </div>
+    </div>
+    <div className="boxes-group">
+      <div className="fake-attachment-box">
+        <ul>
+          {selectedAttachmentsLeft.map((attachment, idx) => (
+            <li key={attachment.id}>
+              {attachment.value}
+              <Button onClick={() => deleteSelectedAttachment(selectedAttachmentsLeft, idx)}>
+Delete
+              </Button>
+            </li>
+))}
+        </ul>
+      </div>
+
+      <div className="fake-attachment-box">
+        <ul>
+          {selectedAttachmentsRight.map((attachment, idx) => (
+            <li key={attachment.id}>
+              {attachment.value}
+              <Button onClick={() => deleteSelectedAttachment(selectedAttachmentsRight, idx)}>
+Delete
+              </Button>
+            </li>
+))}
+        </ul>
+      </div>
+    </div>
+
+    <div className="buttons-group">
+      <ButtonGroup>
+        <Button onClick={() => { copyAttachments(); }}>Copy </Button>
+        <Button> Cancel </Button>
+        <Button> Acept </Button>
+      </ButtonGroup>
+    </div>
+  </React.Fragment>
+);
+};
+
+export default Playground;
